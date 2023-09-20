6b4b515e2b6a2c7b7143d92468aaefd6a7aa1c5c
ajay <ajay@localhost.localdomain>
2023-09-20 12:21:34 -0400

    php tutorial

--- programs.php
+++ programs.php
@@ -1,6 +1,37 @@
 <?php
-//PHP Programs
+//PHP Programs for Beginners
+
+//Programs 01:
+//Check an array is emtpy or not, Show messages
+echo "\n\n";
+
+//Program starting
+//$array = [];
+$array = ["ajay","aryan","geeta","aura","akshita"];
+$count = count($array);
+
+if($count === 0){
+   echo "Empty array \n";
+   echo "Number of elements is $count";
+}else{
+   echo "Array is not empty \n";
+   echo "Number of elements is $count \n";
+   /*
+   echo "Elements filled are: \n";
+   for($i=0; $i<$count; $i++){
+     echo "-> ".$array[$i]."\n";
+   }
+  */
+}
 
 
 
+
+
+
+
+
+
+
+echo "\n\n";
 ?>
