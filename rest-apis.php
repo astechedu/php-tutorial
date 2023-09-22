2baebe7f06d8509b7b3779baa41f2cd68e229a19
ajay <ajay@ubuntu22.04.myguest.virtualbox.org>
2023-09-22 23:48:51 +0530

    php-tutorial from ubuntu22.04

--- rest-apis.php
+++ rest-apis.php
@@ -1,5 +1,7 @@
 
 //api-fetch-all.php
+//Create database, Table: users, Fields: id, student_name, age & city
+
 
 header('Content-Type: application/json');
 header('Access-Conrol-Allow-Origin: *');
@@ -23,6 +25,7 @@ if(mysqli_num_rows($conn, $sql) > 0){
 
  header('Content-Type: application/json');
  header('Access-Control-Allow-Origin: *');
+ 
  $data = json_decode(file_get_contents("php://input"), true);
  $student_id = $data['sid];
 
@@ -92,7 +95,99 @@ if(mysqli_query($conn, $sql){
 }
 
 
-//
-ajay
+//api-delete.php
+
+ header('Content-Type: application/json');
+ header('Access-Control-Allow-Origin: *');
+  header('Access-Control-Allow-Methods: DELETE');
+ header('Access-Control-Allow-Headers: Access-Control-Allow-Methods, Content-Type, Access-Control-Allow-Methods, Autherization, x-Requested-with');
+ 
+ 
+ $data = json_decode(file_get_contents("php://input"), true);
+ $student_id = $data['sid];
+
+include "config.php";
+
+ $sql = "delete from students where id={$squdent_id}";
+ 
+if(mysqli_query($conn, $sql)){
+  echo json_encode(array("message"=>"Student Record Deleted.", "status"=> true));
+}else{
+  echo json_encode(array("message"=>"Student Record Not Deleted.", "status"=> false));
+}
 
 
+
+//api-search.php
+
+ header('Content-Type: application/json');
+ header('Access-Control-Allow-Origin: *');
+ 
+ //$data = json_decode(file_get_contents("php://input"), true);
+ //$search_vlaue = $data['search'];
+
+OR
+  $search_vlaue = ($_GET['search'])?? : die();
+  
+include "config.php";
+
+ $sql = "Select * from students where student_name LIKE '%{$search_value}%'";
+ $result = mysqli_query($conn, $sql) or die("SQL Query Failed.");
+
+if(mysqli_num_rows($conn, $sql) > 0){
+  $output = mysqli_fetch_all($conn, $sql);
+  echo json_encode($output);
+}else{
+  echo json_encode(array("message"=>"No Record Found.", "status"=> false));
+}
+
+
+
+-----------------------------------------------------------------------------------
+
+
+
+
+//Incomplete
+
+//All apis in a single file Using jquery, html
+//video: Yahoo Baba 
+//Incomplete:
+
+
+<script type="">
+<script type="">
+$(document).ready(function(){
+
+  //Featch All Records
+  function loadTable(){
+    $.ajax(function(){
+       url: 'http://localhost/php-rest-api-fetch-all.php',
+       type: "GET",s
+       success: function(data){
+         
+         //console.log(data)
+         
+         if(data.status == false){
+           $("#load_table").append("<tr>..</tr>");
+         }else{
+         
+         }
+       
+       }       
+       
+    });
+  
+  }
+  
+  
+});
+
+
+Incomplete:
+
+Watch video: Yahoo Baba
+php api
+
+
+------------------------------------------------------------------------------------
